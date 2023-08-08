3020a339d1680da376022a059f61d53ee3be36df
luca citta <lucacitta@gmail.com>
2023-08-08 19:50:26 -0300

    [ INIT ] + clase 2

---
+++ Clase_2/clase_2.py
@@ -0,0 +1,47 @@
+print('<---------------------------------->\n\n\n')
+
+# print(type("hola 'que' tal"))
+# print(type('hola "que" tal'))
+# print(type("hola "que" tal"))
+
+# print('Buenas tardes!\nUn gusto saludarte, que necesitas?:')
+# print('Buenas tardes!\nUn gusto saludarte, que necesitas?:')
+
+print('Byte the code'[9]) # comenzamos en 0
+# print('Byte the code'[-4]) # comenzando en -1
+# print('Byte the codeByte the code'[-4]) # comenzando en -1
+
+
+# print('Byte the code'[0:2]) #el inicio es inclusivo el final no lo es 
+# print('Python'[2:6]) #el inicio es inclusivo el final no lo es 
+# print('Python'[2:])
+# print('Python'[:5])
+# print('123456789'[::2])
+
+# print(len('Buenas tardes!'))
+
+# print('Buenas tardes!'[-1])
+
+# print('Buenas tardes!'[len('Buenas tardes!') - 1])
+# print('Buenas tardes!'[14 - 1])
+# print('Buenas tardes!'[13])
+# print('!')
+
+
+
+# nombre_profe = 'luca'
+
+# edad = 20
+
+# print(nombre_profe)
+
+# print(edad)
+
+
+
+
+print('\n\n\n<---------------------------------->')
+
+
+
+
