8d18da27678b364f47c61f96c5b726989ba34086
luca citta <lucacitta@gmail.com>
2023-08-15 20:24:19 -0300

    [ ADD ] Class 4

---
+++ Clase_4/booleans.py
@@ -0,0 +1,112 @@
+print('<---------------------------------->\n\n\n')
+
+
+
+
+
+# print(3.01 == 3)
+# print(True)
+# print(False)
+
+# print(10 != 2)
+
+# print(10 < 10)
+# print(10 > 2)
+
+# print(10 <= 10)
+# print(10 >= 2)
+
+
+# print( 'hola' == "hola")
+# print( 'h "ol" a' == "h 'ol' a")
+# print(20 == 40 / 2)
+# print(str(20.0))
+
+# print(3.0 == 3)
+# print( [1, 2, 'hola'] == list((1, 2, 'hola')) )
+
+
+# print(bool(1))
+# print(bool(0))
+
+# print(bool(''))
+# print(bool('hola'))
+
+# print([] == [])
+# print(bool(['testtt']))
+
+# print(bool(()))
+# print(bool(('testtt')))
+
+# True ** True + False * 10 + 22 * True
+
+# print(True ** True + False * 10 + 22 * True)
+
+# print(True > False)
+
+# print(True > False ** False)
+# print(False ** False)
+
+# print(True > 10/False)
+
+# mi_lista = [1, 2, 3]
+# print(3 in mi_lista)
+# print(mi_lista.index(4))
+
+# print(4 in mi_lista and mi_lista.index(4))
+
+
+# print(2 > 3 or 5 < 10)
+
+# edad = 18
+# permiso = True
+# tenes_carnet = True
+
+# #Te podes hacer un tatu?
+# print(edad >= 18 or permiso)
+
+
+# # Podes manejar
+# print(edad >= 18 and tenes_carnet)
+
+
+# color = 'amarillo'
+# vienen_autos = False
+# # Podes arracan?
+
+# print(color != 'rojo' and vienen_autos == False)
+
+# #Hoy es jueves?
+# hoy = 'jueves'
+# print(not hoy == 'jueves')
+
+
+# color = 'amarillo'
+# vienen_autos = False
+# # Podes arracan?
+
+# print(color != 'rojo' and not vienen_autos)
+# print(10 ** 20)
+
+# print(True)
+
+# edad = 18
+
+# edad += 1 # edad++
+# edad += 1
+
+# edad *= 5
+
+# edad %= 2
+
+# print(edad)
+
+edad = int(input('Ingrese su edad: '))
+nombre = input('Ingrese su nombre: ')
+
+print(nombre, 'sos mayor de edad?', edad >= 18)
+
+
+
+
+print('\n\n\n<---------------------------------->')
