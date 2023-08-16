5d0d04080150f7b686901be66a4b1837f4227b02
luca citta <lucacitta@gmail.com>
2023-08-15 22:03:21 -0300

    [ ADD ] Class 5

---
+++ Clase_5/condicionales.py
@@ -0,0 +1,129 @@
+
+
+print('<---------------------------------->\n\n\n')
+
+
+# edad_programador = 20
+# es_pariente = False
+
+# if edad_programador >= 18 or es_pariente:
+#     print('lo podemos contratar')
+
+
+# edad_programador = 20
+# tiene_antecedentes = False
+
+# if edad_programador >= 18 and not tiene_antecedentes:
+#     print('lo podemos contratar')
+
+# edad = 20
+# es_menor = edad < 18
+
+# if not es_menor:
+#     print('contratado')
+
+
+# edad_programador = 10
+
+# if edad_programador >= 18:
+#     print('Lo podemos contratar')
+# else:
+#     print('No lo podemos contratar')
+
+# luz_semaforo = 'amarilla'
+
+# if luz_semaforo == 'roja':
+#     print('Tenes que frenar')
+
+# elif luz_semaforo == 'amarilla':
+#     print('Pasa pero con cuidado')
+
+# else:
+#     print('Pasa tranquilo')
+
+
+# codigo_llave = 123123
+
+# codigo_puerta = 321321
+
+# if codigo_llave == codigo_puerta:
+#     print('Verde, podes pasar')
+# else:
+#     print('Rojo, te quedas afuera')
+
+
+# codigo_llave_a = 123123
+# codigo_llave_b = 321321 #321321
+
+
+# codigo_puerta_a = [321321, 123123]
+# codigo_puerta_b = 321321
+
+# if codigo_llave_a in codigo_puerta_a:
+#     print('Verde, podes pasar a puerta A')
+# else:
+#     print('Rojo, te quedas afuera en puerta A')
+
+# if codigo_llave_a == codigo_puerta_b:
+#     print('Verde, podes pasar a puerta B')
+# else:
+#     print('Rojo, te quedas afuera en puerta B')
+
+
+# comando = "ENTRAR"
+
+# if comando == "ENTRAR":
+#     print("Bienvenido al sistema.")
+# elif comando == "SALUDO":
+#     print("Hola! ¿Cómo estás?")
+# elif comando == "SALIR":
+#     print("Saliendo del sistema.")
+# else:
+#     print("No se reconoce el comando.")
+
+# if comando == "ENTRAR":
+#     print("Bienvenido al sistema.")
+# if comando == "SALUDO":
+#     print("Hola! ¿Cómo estás?")
+# if comando == "SALIR":
+#     print("Saliendo del sistema.")
+# else:
+#     print("No se reconoce el comando.")
+
+
+# edad = 1
+# if edad >= 18:
+#     print('lo podemos contratar')
+# else:
+#     print('no lo podemos contratar')
+
+# print('lo podemos contratar') if edad >= 18 else print('no lo podemos contratar')
+
+# es_mayor = True if edad >= 18 else False
+
+
+# edad_programador = 20
+# tiene_antecedentes = False
+# es_pelado = True
+
+# if edad_programador >= 18:
+#     if not tiene_antecedentes:
+#         if not es_pelado:
+#             print('lo podemos contratar')
+#         else:
+#             # LE DAMOS UN VOUCHER DE IMPLANTE CAPILAR
+#             print('no lo podemos contratar, es pelado')
+#     else:
+#         # LO AGREGAMOS A LA BASE DE DATOS Y CUANDO PASEN 5 AÑOS LO LLAMAMOS
+#         print('no lo podemos contratar, tiene antecedentes')
+# else:
+#     # LO AGREGAMOS A LA BASE DE DATOS Y CUANDO TENGA 18 LO VOLVEMOS A LLAMAR
+#     print('no lo podemos contratar, es menor')
+
+# if edad_programador >= 18 and not tiene_antecedentes and not es_pelado:
+#     print('lo podemos contratar')
+# else:
+#     print('no lo podemos contratar')
+
+
+print('\n\n\n<---------------------------------->')
