ae358cb1aee9b6dab143a320b7a53abcdc95a660
luca citta <lucacitta@gmail.com>
2023-08-24 22:02:10 -0300

    [ ADD ] Class 8

---
+++ Clase_8/conjuntos.py
@@ -0,0 +1,100 @@
+print('<---------------------------------->\n\n\n')
+
+
+# mi_conjunto = {1,2,3}
+# print(mi_conjunto)
+# print(type(mi_conjunto))
+
+# mi_conjunto = {1, 2, 3, (4, 5, 6)}
+# # mi_conjunto = {1, 2, 3, [4, 5, 6]}
+# mi_conjunto = {1, 2, 3, {4, 5, 6}}
+# print(mi_conjunto)
+
+
+# lista = [1, 4, 2, 9, 4, 3]
+# nueva_lista = []
+
+# for elemento in lista:
+#     if elemento not in nueva_lista:
+#         nueva_lista.append(elemento)
+
+# print(nueva_lista)
+
+# print(set(lista))
+
+
+# mi_conjunto = {1,2,3}
+# mi_lista = [3,4,5,6]
+# mi_conjunto.update(mi_lista)
+# mi_conjunto.discard(1)
+# mi_conjunto.remove(1)
+# print(mi_conjunto)
+
+# if 3 in mi_conjunto:
+#     print('El 3 esta aca!!')
+# else:
+#     print('No lo encontre :(')
+
+# print(mi_conjunto.pop())
+
+
+
+# profe_list = ['luca', '24', 'claros']
+
+# profe = {
+#     'nombre':'luca',
+#     'edad':'24',
+#     'ojos':'claros'
+# }
+
+# profe['ojos'] = 'oscuros'
+# profe_list[2] = 'oscuros'
+
+# print(f'El profe se llama {profe["nombre"]}, tiene {profe["edad"]} y unos bellos ojos {profe["ojos"]}')
+
+# print(f'El profe se llama {profe_list[0]}, tiene {profe_list[1]} y unos bellos ojos {profe_list[2]}')
+
+
+# alumnos = [
+#     {
+#         'nombre':'Lisando',
+#         'apellido':'Ratto',
+#         'edad': 22,
+#     },
+#     {
+#         'nombre':'Junior',
+#         'apellido':'Torres',
+#         'edad': 24,
+#     },
+#     {
+#         'nombre':'Claudio',
+#         'apellido':'Moreno',
+#         'edad': 25,
+#     }
+# ]
+
+
+# print(alumnos[2]['nombre'])
+
+
+# colores ={
+#     'rojo':'red',
+#     'amarillo':'yellow',
+#     'verde':'green'
+# }
+
+# nuevos_colores = {
+#     'azul':'blue',
+#     'esmeralda':'emerald'
+# }
+# colores.update(nuevos_colores)
+# colores['violeta'] = 'purple'
+# print(colores['violeta'])
+# print(colores)
+# colores.pop('esmeral123da', '')
+# print(colores)
+# print(colores.keys())
+# print(colores.values())
+
+
+print('\n\n\n<---------------------------------->')
