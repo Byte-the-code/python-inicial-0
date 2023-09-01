3cab2d8fa8c0808be469cf3d6f8387abc00a2c65
luca citta <lucacitta@gmail.com>
2023-08-31 22:03:53 -0300

    [ ADD ] Class 10

---
+++ Clase_10/funciones.py
@@ -0,0 +1,93 @@
+print('<---------------------------------->\n\n\n')
+
+# def custom_len(elemento_a_iterar):
+#     contador = 0
+#     for elemento in elemento_a_iterar:
+#         contador += 1
+#     return contador
+
+
+# iterable1 = 'palabra_de_test'
+
+# resultado = custom_len(iterable1)
+# print(resultado)
+
+
+# iterable2 = 'Este es otro ejemplo'
+
+# resultado = custom_len(iterable2)
+# print(resultado)
+
+
+# iterable3 = 'Dale, vamos con uno mas'
+
+# resultado = custom_len(iterable3)
+# print(resultado)
+
+
+# def saludo_byte(nombre, apellido, edad):
+#     return f'Hola {nombre} {apellido} de {edad} años desde Byte The Code!'
+
+# print(saludo_byte('luca', 'citta', 23))
+
+
+# def ingresar_al_boliche(asistencia):
+#     nombre = input('Ingrese el nombre : ')
+#     horario = 2
+#     edad = int(input('Ingresa tu edad'))
+#     if edad > 18:
+#         if horario == 2:
+#             print('cobrale 200 pesos')
+#         elif horario == 4:
+#             pass
+#         elif horario > 5:
+#             pass
+#     else:
+#         print('sos menor')
+
+#     print('cobrale 100 pesos')
+#     asistencia.append(nombre)
+#     return asistencia
+
+
+# asistencia = []
+# hay_alguien_mas = 'si'
+
+# while hay_alguien_mas == 'si':
+#     asistencia = ingresar_al_boliche(asistencia)
+#     hay_alguien_mas = input('Hay alguien mas? (si/no): ')
+
+
+
+
+# def suma(num_1, num_2):
+#     num_1 = numero_1
+#     num_1 += 50
+#     print(num_1 + num_2)
+
+
+# numero_1 = 10
+# numero_2 = 20
+# suma(numero_1, numero_2)
+
+# print(numero_1)
+
+
+def agregar_nuevo_inscripto(listado, alumno):
+    listado = listado_alumnos
+    alumno = alumno_nuevo
+    listado.append(alumno)
+
+listado_alumnos = ['Claudio', 'Lorenzo']
+alumno_nuevo = 'Lisandro'
+agregar_nuevo_inscripto(listado_alumnos, alumno_nuevo)
+
+print(listado_alumnos)
+
+
+
+
+
+
+
+print('\n\n\n<---------------------------------->')
